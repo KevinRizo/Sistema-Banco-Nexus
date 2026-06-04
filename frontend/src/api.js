d6fbad22099d1ceb408a8820f3b3a0d288deb4c5
Kevin Rizo Ponce <k3ripo@gmail.com>
2026-06-04 01:56:24 -0700

    Corrige URL del backend en producción

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.REACT_APP_API_URL ||
-  'http://localhost:3000';
+  'http://ec2-54-90-218-159.compute-1.amazonaws.com:3000';
 
 export const apiUrl = (ruta) =>
   `${API_URL}${ruta}`;
