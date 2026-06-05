abcbcfc3ddb13fffda8ed9fa359c498685c1f820
Kevin Rizo Ponce <k3ripo@gmail.com>
2026-06-04 21:03:51 -0700

    Configura Elastic IP para backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.REACT_APP_API_URL ||
-   'http://ec2-54-80-87-19.compute-1.amazonaws.com:3000';
+  'http://44.207.245.101:3000';
 
 export const apiUrl = (ruta) =>
   `${API_URL}${ruta}`;
