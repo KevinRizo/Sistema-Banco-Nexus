c0a76a0269e6bf837e0842cdad2aabd60ac5fea5
Kevin Rizo Ponce <k3ripo@gmail.com>
2026-06-05 05:40:22 -0700

    Usar ALB como backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.REACT_APP_API_URL ||
-  'http://44.207.245.101:3000';
+  'http://Balanceador-Banco-1515816684.us-east-1.elb.amazonaws.com';
 
 export const apiUrl = (ruta) =>
   `${API_URL}${ruta}`;
